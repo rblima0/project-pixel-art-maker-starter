0ecd611dd5049e2f3ff4be946dd96288f831db15
Rodrigo Banci <rblima0@gmail.com>
2018-01-08 19:45:27 -0200

    Adicionando acoes ao projeto

--- designs.js
+++ designs.js
@@ -2,9 +2,40 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+let height, width, color, tableCanvas, pixelCanvas;
 
-function makeGrid() {
+$('#sizePicker').submit(function(e) {
+    e.preventDefault();
 
-// Your code goes here!
+    height = $('#input_height').val();
+    width = $('#input_width').val();
+    pixelCanvas = $('#pixel_canvas');
+    tableCanvas = '';
 
+    makeGrid(height, width);
+    colorClick();
+});
+
+function makeGrid(height, width) {
+    if (pixelCanvas.children().length) {
+        pixelCanvas.html(tableCanvas);
+    }
+
+    for (var i = 0; i < height; i++) {
+        tableCanvas += "<tr>";
+        for (let x = 0; x < width; x++) {
+            tableCanvas += "<td></td>";
+        }
+        tableCanvas += "</tr>";
+    }
+    
+    pixelCanvas.html(tableCanvas);
+    pixelCanvas.css("background-color", "#fff");
+};
+
+function colorClick(){
+    $('#pixel_canvas').click(function(e) {
+        color = $('#colorPicker').val();
+        $(e.target).css('background-color', color);
+    });
 }
